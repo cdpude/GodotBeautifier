702bbcfec0c6c3e357933ae2f98169895c4d989d
cdpude <cdpude@gmail.com>
2023-02-24 02:19:52 +0800

    Add cool black theme

---
+++ addons/godot_beautifier/themes/Cool Black/theme.gd
@@ -0,0 +1,17 @@
+extends BeautifierAPI
+
+
+func _enter_tree():
+	set_editor_setting("interface/theme/icon_and_font_color", 0)
+	set_editor_setting("interface/theme/base_color", Color("e1292825"))
+	set_editor_setting("interface/theme/accent_color", Color("bfb68a3d"))
+	set_editor_setting("interface/theme/custom_theme", get_system_file("assets/theme.tres"))
+	set_editor_setting("interface/editor/code_font", get_system_file("assets/SpaceMono-Regular.ttf"))
+	set_editor_setting("interface/editor/dim_editor_on_dialog_popup", false)
+	set_editor_setting("text_editor/theme/line_spacing", 4)
+	set_editor_setting("text_editor/indent/size", 4)
+	set_editor_setting("text_editor/indent/draw_tabs", false)
+	
+	set_text_editor_colors_by_cfg(get_file("assets/Cool-Black.tet"))
+	
+	set_project_setting("rendering/environment/default_clear_color", Color("#202020"))
