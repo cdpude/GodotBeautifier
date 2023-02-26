016a4ef22c04dc2953d86e0066caf7401d925bc8
cdpude <cdpude@gmail.com>
2023-02-27 03:03:26 +0800

    Add tokyo night theme

---
+++ addons/godot_beautifier/themes/Tokyo Night/theme.gd
@@ -0,0 +1,15 @@
+extends BeautifierAPI
+
+
+func _enter_tree():
+	set_editor_setting("interface/theme/base_color", Color("181822"))
+	set_editor_setting("interface/theme/accent_color", Color("6e738e"))
+	set_editor_setting("interface/theme/additional_spacing", 4)
+	set_editor_setting("interface/theme/border_size", 1)
+	set_editor_setting("interface/theme/custom_theme", get_system_file("assets/theme.tres"))
+	set_editor_setting("interface/editor/code_font", get_system_file("assets/lucidatypewriterregular.ttf"))
+	set_editor_setting("text_editor/script_list/current_script_background_color", Color.transparent)
+	
+	set_text_editor_colors_by_cfg(get_file("assets/Tokyo-Night.tet"))
+	
+	set_project_setting("rendering/environment/default_clear_color", Color("1a1b26"))
